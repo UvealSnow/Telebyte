8b36499812afae64df1d10ef2d828db23df37835
Kevin Avila <kevin.avila@avantgarde.de>
2021-09-06 16:10:10 +0200

    formspree & recaptcha

--- src/main.js
+++ src/main.js
@@ -3,11 +3,32 @@
 
 require('~/assets/scss/main.scss');
 
-import Icon from '~/components/atoms/Icon.vue';
-import DefaultLayout from '~/layouts/Default.vue'
 
-export default function (Vue, { router, head, isClient }) {
+import Icon from '~/components/atoms/Icon.vue';
+import DefaultLayout from '~/layouts/Default.vue';
+
+import { extend } from 'vee-validate';
+import { required, email } from 'vee-validate/dist/rules';
+
+extend('email', {
+  ...email,
+  message: 'Por favor ingresa un correo electrónico válido.',
+});
+
+extend('required',{
+  ...required,
+  message: 'Este campo es requerido.',
+});
+
+export default function (Vue, { head }) {
   // Set default layout as a global component
   Vue.component('Icon', Icon);
   Vue.component('Layout', DefaultLayout);
+
+  head.script.push({
+    src: 'https://www.google.com/recaptcha/api.js',
+    body: true,
+    async: true,
+    defer: true,
+  });
 }
