c618033d204fb1c9ab488b26856c54d5d7a285b7
Kevin Avila <kevin.avila@avantgarde.de>
2021-09-07 17:30:57 +0200

    Services page

---
+++ src/pages/Services.vue
@@ -0,0 +1,20 @@
+<template>
+  <Layout>
+    <hero
+      :section="{
+        title: 'Servicios',
+        subtitle: 'Encuentra el servicio que tu empresa requiere'
+      }"
+    />
+  </Layout>
+</template>
+
+<script>
+import Hero from '../components/sections/Hero.vue';
+
+export default {
+  components: {
+    Hero,
+  },
+};
+</script>
