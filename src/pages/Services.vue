a84b81155b6797436e7f4e95e1af26ec91c26e5c
Kevin Avila <kevin.avila@avantgarde.de>
2021-09-08 09:52:30 +0200

    under construction - services

--- src/pages/Services.vue
+++ src/pages/Services.vue
@@ -6,6 +6,11 @@
         subtitle: 'Encuentra el servicio que tu empresa requiere'
       }"
     />
+
+    <div class="container mx-auto">
+      <p class="subtitle text-center mt-8">Vuelve pronto</p>
+      <h1 class="title text-4xl text-center">En construcción</h1>
+    </div>
   </Layout>
 </template>
 
@@ -18,3 +23,16 @@ export default {
   },
 };
 </script>
+
+<static-query>
+{
+  gcms {
+    services(orderBy: order_ASC) {
+      title
+      slug
+      brief
+      icon
+    }
+  }
+}
+</static-query>
