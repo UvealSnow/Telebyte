380407cd2b78d33f75b39f049041f09078b99ee6
Kevin Avila <kevin.avila@avantgarde.de>
2021-09-08 10:29:59 +0200

    pages for posts and services

---
+++ src/components/PostInfo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex flex-row text-xs justify-between select-none">
+    <p>por <span class="font-bold" v-text="writtenBy" /></p>
+    <p v-text="publishedAt" />
+  </div>
+</template>
+
+<script>
+import * as moment from 'moment';
+
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true,
+      default: () => ({}),
+    },
+  },
+
+  computed: {
+    writtenBy() {
+      return this.post.createdBy.name.replace(' ', '_').toLowerCase();
+    },
+
+    publishedAt() {
+      return moment(this.post.publishedAt).format('LL');
+    },
+  },
+};
+</script>
