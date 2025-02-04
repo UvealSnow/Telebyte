f7d398d7feb49b1163dc33790c0c3eea2f41a805
Kevin Avila <kevhah92@gmail.com>
2025-02-04 20:16:40 +0100

    pages (feat) - Add pages templete, update gridsome.server.js (#57)
    Co-authored-by: Kevin Avila <kevin.avila@avantgarde.de>

---
+++ src/templates/Page.vue
@@ -0,0 +1,35 @@
+<template>
+  <Layout>
+
+    <div class="container mx-auto my-48">
+      <h1 class="font-bold text-4xl pb-2" v-text="page.title" />
+      <p class="text-lg font-light" v-text="page.brief" />
+
+      <div class="my-8" v-html="page.body.html" />
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  computed: {
+    page() {
+      return this.$page.gcms.page;
+    },
+  },
+}
+</script>
+
+<page-query>
+  query getPage($slug: String) {
+    gcms {
+      page(where: {slug: $slug}) {
+        title
+        brief
+        body {
+          html
+        }
+      }
+    }
+  }
+</page-query>
